eacf6d614bde7c2e48d4afa100f29db4da73941f
MarciaPoel <marciavdpoel@gmail.com>
2024-06-03 11:38:52 +0200

    Update Patient_data.py

--- latest_code/Patient_data.py
+++ latest_code/Patient_data.py
@@ -17,15 +17,3 @@ def generate_random_patient():
     weight = round(random.uniform(50, 150), 1)
     motivation = random.randint(0, 5)  # TTM framework stages
     return Patient(age, years_T2DM, physical_activity, glucose_level, weight, motivation)
-
-patients = [generate_random_patient() for _ in range(3)]
-
-for patient in patients:
-   print("Patient Information:")
-   print(f"Age: {patient.age}")
-   print(f"Years of T2DM: {patient.years_T2DM} years")
-   print(f"Physical Activity: {patient.physical_activity}")
-   print(f"Glucose Level: {patient.glucose_level} mg/dL")
-   print(f"Weight: {patient.weight} kg")
-   print(f"Motivation: {patient.motivation}")
-   print("----------------------")
